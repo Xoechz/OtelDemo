814f31c2d9bc277f4a629cf9644681dc11c03017
Elias Leonhardsberger <elias.leonhardsberger@gmail.com>
2026-01-07 17:29:07 +0100

    added a container dito instance that targets grafana

--- src/Demo.AppHost/Program.cs
+++ src/Demo.AppHost/Program.cs
@@ -9,10 +9,22 @@ const string OTEL_COLLECTOR_ENDPOINT = "http://localhost:4317";
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddContainer("lgtm", "grafana/otel-lgtm")
+    .WithLifetime(ContainerLifetime.Persistent)
     .WithIconName("Eye")
-    .WithEndpoint(24317, 4317, "http", "otel-grpc") // OTLP gRPC
-    .WithEndpoint(24318, 4318, "http", "otel-http") // OTLP HTTP
-    .WithEndpoint(3000, 3000, "http", "grafana-ui"); // Grafana UI
+    .WithEndpoint(24317, 4317, "http", "otel-grpc")
+    .WithEndpoint(24318, 4318, "http", "otel-http")
+    .WithEndpoint(3000, 3000, "http", "grafana-ui");
+
+// There is currently an issue that the collector cannot send telemetry to the apphost, so only grafana is receiving telemetry.
+var dito = builder.AddContainer("dito", "xoechz/dito")
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WithIconName("ShowGrid")
+    .WithEndpoint(4317, 4317, "http", "otel-grpc")
+    .WithEndpoint(4318, 4318, "http", "otel-http")
+    .WithEndpoint(13133, 13133, "http", "health-check")
+    .WithEndpoint(55679, 55679, "http", "zpages")
+    .WithEndpoint(1777, 1777, "http", "pprof")
+    .WithHttpHealthCheck(endpointName: "health-check");
 
 var sql = builder.AddSqlServer("sql")
     .WithLifetime(ContainerLifetime.Persistent)
